ac8fa172d3fc727f6a74ecc0ccd654dee2d9cf3c
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-11-19 14:26:38 -0500

    Remove print

--- manifest-from-input-json/handler.py
+++ manifest-from-input-json/handler.py
@@ -6,10 +6,7 @@ from processJson import processJson
 
 
 def run(event, context):
-    processSet = processJson()
-    print("yo")
-    print(event.get("data")['unique-identifier'])
-    
+    processSet = processJson()    
     processSet._set_file_data("unused", "unused", "unused")
     processSet._create_manifest_json(event.get("data"))
     event.update( { "manifest": processSet.result_json })
