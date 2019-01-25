149e25b88108cf19e1d2722390f518c5e7e0aa05
Ryan Doughty <rdought1@nd.edu>
2019-01-25 16:59:29 -0500

    saveLastRun was not copying the images.  now it does

--- finalize/finalizeStep.py
+++ finalize/finalizeStep.py
@@ -64,20 +64,42 @@ class finalizeStep():
         print("Save Last Run")
         s3 = boto3.resource('s3')
 
-        from_bucket = s3.Bucket(self.config["process-bucket"])
-        to_bucket = s3.Bucket(self.config["process-bucket"])
-
         from_path = self.config["process-bucket-read-basepath"] + "/" + self.id + "/"
         # all the items in the process directory for the id
-        all_objects = from_bucket.object_versions.filter(Prefix=from_path, Delimiter="/")
+        try:
+            objects = self._list_s3_obj_by_dir(from_path,self.config["process-bucket"])
 
-        for o in all_objects:
-            copy_source = {
-                'Bucket': self.config["process-bucket"],
-                'Key': o.object_key
-            }
-            to_bucket.copy(copy_source, self.config["process-bucket-write-basepath"] + "/" + self.id + "/lastSuccessfullRun/" + os.path.basename(o.object_key))
-        return
+            for s3obj in objects:
+                copy_src = {'Bucket':self.config["process-bucket"],'Key':s3obj}
+                dest_key = self.config["process-bucket-write-basepath"] + "/" \
+                            + self.id + "/lastSuccessfullRun/" + s3obj[len(from_path):]
+                s3.Object(self.config["process-bucket"],dest_key).copy_from(CopySource=copy_src)
+        except Exception as e:
+            print(e)
+
+    def _list_s3_obj_by_dir(self, s3dir, bucket):
+        s3 = boto3.client('s3')
+        params = {
+            'Bucket': bucket,
+            'Prefix': s3dir,
+            'StartAfter': s3dir,
+        }
+        attempt = 0
+        keys = []
+        while ('ContinuationToken' in params) or (attempt == 0):
+            attempt += 1
+            objects = s3.list_objects_v2(**params)
+            for content in objects['Contents']:
+                # skip 'folders'
+                if content['Key'].endswith('/'):
+                    continue
+                keys.append(content['Key'])
+            # grab more objects to process if necessary(max 1,000/request)
+            if objects['IsTruncated']:
+                params['ContinuationToken'] = objects['NextContinuationToken']
+            else:
+                params.pop('ContinuationToken', None)
+        return keys
 
     def saveIndexMetadata(self):
         print("Save Index Metadata")
