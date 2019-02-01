5ac1c024cd7cbd143b47413aecbae7d39788b723
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-01 16:21:34 -0500

    Last try

--- scripts/codebuild/install.sh
+++ scripts/codebuild/install.sh
@@ -2,9 +2,11 @@
 echo "[Install phase] `date` in `pwd`"
 
 cd pyramid-generator
-#npm install --only=prod
+npm install --only=prod
 
 echo "Installing LINUX sharp/libvips packages"
 export SHARP_IGNORE_GLOBAL_LIBVIPS=true
 rm -rf node_modules/sharp
 npm install --arch=x64 --platform=linux --target=8.10.0 sharp
+
+find ./node_modules -mtime +10950 -exec touch {} \;
