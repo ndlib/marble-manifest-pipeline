b3edc4eea24acd9c9a2f04aac318f3ddb41961ee
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-01 16:19:10 -0500

    One last try

--- scripts/codebuild/install.sh
+++ scripts/codebuild/install.sh
@@ -2,7 +2,7 @@
 echo "[Install phase] `date` in `pwd`"
 
 cd pyramid-generator
-npm install --only=prod
+#npm install --only=prod
 
 echo "Installing LINUX sharp/libvips packages"
 export SHARP_IGNORE_GLOBAL_LIBVIPS=true
