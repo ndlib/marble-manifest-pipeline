102f8292d8b1e95ff56b1f6ed1bc5a8fe7eb906b
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-04 10:33:07 -0500

    Remove build spec and try to change the pipeline to be more accessible

--- scripts/codebuild/install.sh
+++ scripts/codebuild/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "[Install phase] `date` in `pwd`"
 
+echo "Installing LINUX sharp/libvips packages"
 cd pyramid-generator
 npm install --only=prod
 
-echo "Installing LINUX sharp/libvips packages"
 export SHARP_IGNORE_GLOBAL_LIBVIPS=true
 rm -rf node_modules/sharp
 npm install --arch=x64 --platform=linux --target=8.10.0 sharp
 
+# fixes incorrect created files from the extract.
 find ./node_modules -mtime +10950 -exec touch {} \;
