c617cc766bd951407585e3c5220d4b07f674200e
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-04 14:45:37 -0500

    Update build scripts to try to get the pipeline working again.

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -4,7 +4,7 @@ echo "S3 Bucket"
 echo $S3_BUCKET
 
 echo "Copy the resource.yml to this project so it can run"
-cp $FROM_PATH $TO_PATH
+cp $CODEBUILD_SRC_DIR_ConfigCode/deploy/cloudformation/manifest-pipeline.yml $CODEBUILD_SRC_DIR/
 
 echo "Aws Package Command"
 echo "aws cloudformation package --template-file manifest-pipeline.yml --s3-bucket $S3_BUCKET --output-template-file output.yml"
