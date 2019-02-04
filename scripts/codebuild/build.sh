3e1955c4e3e7d15c9e0af07da51c52420dbf83ee
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-04 14:14:40 -0500

    Update to the pipeline

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -4,9 +4,8 @@ echo "S3 Bucket"
 echo $S3_BUCKET
 
 echo "Copy the resource.yml to this project so it can run"
-# cp $BLUPRINTS_DIRECTORY/deploy/cloudformation/resources.yml $CODE_DIRECTORY/
-wget https://raw.githubusercontent.com/ndlib/mellon-manifest-pipeline/changes-to-cf-to-make-it-pipeline-ready/deploy/resources.yml
+cp $FROM_PATH $TO_PATH
 
 echo "Aws Package Command"
-echo "aws cloudformation package --template-file resources.yml --s3-bucket $S3_BUCKET --output-template-file output.yml"
-aws cloudformation package --template-file resources.yml --s3-bucket $S3_BUCKET --output-template-file output.yml
+echo "aws cloudformation package --template-file manifest-pipeline.yml --s3-bucket $S3_BUCKET --output-template-file output.yml"
+aws cloudformation package --template-file manifest-pipeline.yml --s3-bucket $S3_BUCKET --output-template-file output.yml
