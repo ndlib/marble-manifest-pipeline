b7ac24402ceb8820bcfb13e97d3ae86778927483
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-02-05 15:28:36 -0500

    Change the location of the copy.

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -3,9 +3,6 @@ echo "[Build phase] `date` in `pwd`"
 echo "S3 Bucket"
 echo $S3_BUCKET
 
-echo "Copy the resource.yml to this project so it can run"
-cp $CODEBUILD_SRC_DIR_ConfigCode/deploy/cloudformation/manifest-pipeline.yml $CODEBUILD_SRC_DIR/
-
 echo "Aws Package Command"
 echo "aws cloudformation package --template-file manifest-pipeline.yml --s3-bucket $S3_BUCKET --output-template-file output.yml"
 aws cloudformation package --template-file manifest-pipeline.yml --s3-bucket $S3_BUCKET --output-template-file output.yml
--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -1,2 +1,4 @@
-
 echo "[Pre-Build phase] `date` in `pwd`"
+
+echo "Copy the resource.yml to this project so it can run"
+cp $CODEBUILD_SRC_DIR_ConfigCode/deploy/cloudformation/manifest-pipeline.yml $CODEBUILD_SRC_DIR/
