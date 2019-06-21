6b3d9380bd130e84fdec9718b861b2aff0d28359
jmille79 <jmille79@nd.edu>
2019-06-21 10:21:53 -0400

    formatting changes

--- manifestpy/mapmain.py
+++ manifestpy/mapmain.py
@@ -12,14 +12,22 @@ def mapMainManifest(readfile, wtype):
     while i < metl:
         label = readfile['metadata'][i]['label']
         value = readfile['metadata'][i]['value']
-         if label == 'Keywords': mainOut += '"keywords":"' + value + '",'
-         if label == 'Alternate Title': mainOut += '"alternateName":"' + value + '",'
-         if label == 'Creation Date': mainOut += '"dateCreated":"' + value + '",'
-         if label in ('Accession Number', 'Rare Books Identifier', 'Identifier'): mainOut += '"identifier":"' + value + '",'
-         if label in ('Artist','Author','Creator'): mainOut += '"creator":"' + value + '",'
-         if label in ('Date', 'Dates'): mainOut += '"temporalCoverage":"' + value + '",'
-         if label in ('Language of Materials', 'Language'): mainOut += '"inLanguage":"' + value +'",'
-         if label in ('Media', 'Format'): mainOut += '"matierial":"' + value + '",'
-         i += 1
+        if label == 'Keywords':
+            mainOut += '"keywords":"' + value + '",'
+        if label == 'Alternate Title':
+            mainOut += '"alternateName":"' + value + '",'
+        if label == 'Creation Date':
+            mainOut += '"dateCreated":"' + value + '",'
+        if label in ('Accession Number', 'Rare Books Identifier', 'Identifier'):
+            mainOut += '"identifier":"' + value + '",'
+        if label in ('Artist','Author','Creator'):
+            mainOut += '"creator":"' + value + '",'
+        if label in ('Date', 'Dates'):
+            mainOut += '"temporalCoverage":"' + value + '",'
+        if label in ('Language of Materials', 'Language'):
+            mainOut += '"inLanguage":"' + value +'",'
+        if label in ('Media', 'Format'):
+            mainOut += '"matierial":"' + value + '",'
+        i += 1
 
     return mainOut
