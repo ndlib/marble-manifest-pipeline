662381857b40543dbc48f7ba685e1beb5037238e
Jon Hartzler <jon.hartzler@gmail.com>
2019-07-17 09:45:43 -0400

    add comments

--- fix_csv_for_3.py
+++ fix_csv_for_3.py
@@ -1,13 +1,11 @@
+# edit bucket below to be your bucket
+# assume aws creds for your account
+# usage python fix_csv_for_3.py
+# NOTE: this will start the step functions when it uploads the mains.csv
+#
 import boto3
 import os
 
-# get files from version
-# copy sequence to items
-# open main
-# change License -> Rights
-# change Description -> Summary
-# put file back up
-# remove sequnece.csv
 s3 = boto3.resource('s3')
 
 bucket = "manifest-pipeline-v3-processbucket-kmnll9wpj2h3"
@@ -47,8 +45,8 @@ for o in result.get('CommonPrefixes'):
 
     os.rename(local_sequence, local_item)
 
-    s3.meta.client.upload_file(local_main, bucket, main)
     s3.meta.client.upload_file(local_item, bucket, items)
+    s3.meta.client.upload_file(local_main, bucket, main)    
 
 # s3.Object(bucket, sequence).delete()
 os.rmdir('./fix')
