3c707557a73e98bb0624a57efce7c7426a84c90d
jmille79 <jmille79@nd.edu>
2019-07-30 16:08:21 -0400

    Create METS to Schema conversion

---
+++ manifestpy/mapmets.py
@@ -0,0 +1,19 @@
+def main():
+    import os
+    import xmltodict
+    import json
+    from mapmetsmain import mapMetsManifest
+    path = os.path.dirname(os.path.abspath(__file__))
+    rfile = os.path.join(path, '1957.058.xml')
+    with open(rfile, 'r') as f:
+        readfile = xmltodict.parse(f.read())
+    print readfile
+    mainOut = mapMetsManifest(readfile, 'CreativeWork')
+    wfile = os.path.join(path, 'outputmainmets.json')
+    writemain = open(wfile, 'w+')
+    writemain.write(json.dumps(mainOut))
+    return
+
+
+if __name__ == '__main__':
+    main()
