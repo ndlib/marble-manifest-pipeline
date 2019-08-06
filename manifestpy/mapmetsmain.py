d7d62d5dad62538958f3951d00a91c0bf5383651
jmille79 <jmille79@nd.edu>
2019-08-06 12:55:29 -0400

    Applied suggested changes

--- manifestpy/mapmetsmain.py
+++ manifestpy/mapmetsmain.py
@@ -2,31 +2,33 @@ import json
 
 
 def mapMetsManifest(readfile, wtype):
-    xmlData = readfile['mets:mets']['mets:dmdSec'][0]['mets:mdWrap']['mets:xmlData']
-    xmlVRA = readfile['mets:mets']['mets:dmdSec'][1]['mets:mdWrap']['mets:xmlData']['vracore:work']
+    xmlBase = readfile['mets:mets']['mets:dmdSec']
+    for i in xmlBase:
+        if i['@ID'] == 'DSC_01_SNITE':
+            xmlDC = i['mets:mdWrap']['mets:xmlData']
+        if i['@ID'] == 'DSC_02_SNITE':
+            xmlVRA = i['mets:mdWrap']['mets:xmlData']['vracore:work']
     mainOut = {
         '@context': 'http://schema.org',
         'type': wtype,
-        'name': json.dumps(xmlData['dcterms:title']),
-        'provider': json.dumps(xmlData['dcterms:publisher']),
-        'license': json.dumps(xmlData['dcterms:rights']),
-        "description": json.dumps(xmlData['dcterms:provenance']),
-        "alternateName": json.dumps(xmlData['dcterms:accessRights']),
-        "identifier": json.dumps(xmlData['dcterms:identifier']),
-        "creator": json.dumps(xmlData['dcterms:creator']),
-        "temporalCoverage": json.dumps(xmlData['dcterms:created']),
-        "inLanguage": json.dumps(xmlVRA['vracore:agentSet']['vracore:culture']),
+        'name': json.dumps(xmlDC['dcterms:title']),
+        'provider': json.dumps(xmlDC['dcterms:publisher']),
+        'license': json.dumps(xmlDC['dcterms:rights']),
+        "description": json.dumps(xmlDC['dcterms:provenance']),
+        "alternateName": json.dumps(xmlDC['dcterms:accessRights']),
+        "identifier": json.dumps(xmlDC['dcterms:identifier']),
+        "creator": json.dumps(xmlDC['dcterms:creator']),
+        "temporalCoverage": json.dumps(xmlDC['dcterms:created']),
         "material": json.dumps(xmlVRA['vracore:materialSet']['vracore:display'])
     }
     for k, v in mainOut.items():
         mainOut[k] = mainOut[k].strip('\"')
-    if 'dcterms:subject' in xmlData.keys():
-        kw = json.dumps(xmlData['dcterms:subject'])
+    if 'dcterms:subject' in xmlDC.keys():
+        kw = json.dumps(xmlDC['dcterms:subject'])
         kw = kw.replace('\"', '')
         kw = kw.strip('[')
         kw = kw.strip(']')
         kw = kw.split(',')
-        print kw
         mainOut["keywords"] = kw
 
     return mainOut
