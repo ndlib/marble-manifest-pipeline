f6df67599f632e68312b40e68d78972b4997b20e
jmille79 <jmille79@nd.edu>
2019-08-28 10:54:10 -0400

    Updated formatting, testing changes

--- manifestpy/test_manifestmap.py
+++ manifestpy/test_manifestmap.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import json
 from manifestmap import mapManifestOfItems
 
 
@@ -7,32 +8,14 @@ class testManifestMap(unittest.TestCase):
     def test(self):
         path = os.path.dirname(os.path.abspath(__file__))
         opchild = os.path.join(path, 'outputchild0.json')
-        testReadfile = {
-          'type': 'Manifest',
-          'label': 'Test',
-          'author': 'Unknown',
-          'identifier': 'abc-123',
-          'sequences': [{
-              'canvases': [{
-                  'label': 'cvsNameTest',
-                  '@id': 'http://cvstesturl.com',
-                  'thumbnail': {
-                    'label': 'thumbTest',
-                    '@id': 'http://thumbtesturl.com'
-                  }
-              }]
-          }]
-        }
-        testShouldBe = {
-          '@context': 'http://schema.org',
-          '@type': 'CreativeWork',
-          u'name': 'Test',
-          u'creator': 'Unknown',
-          u'identifier': 'abc-123',
-          u'hasPart': [str(opchild)]
-        }
-        self.assertEqual(mapManifestOfItems(testReadfile, 'CreativeWork'), testShouldBe)
-        self.assertNotEqual(mapManifestOfItems(testReadfile, 'CreativeWorkSeries'), testShouldBe)
+
+        with open("test_data.json", 'r') as input_source:
+            test_readfile = json.load(input_source)
+        input_source.close()
+
+        test_should_be = {'@context': 'http://schema.org', '@type': 'CreativeWork', u'name': 'Test', u'creator': 'Unknown', u'identifier': 'abc-123', u'hasPart': [str(opchild)]}
+        self.assertEqual(mapManifestOfItems(test_readfile, 'CreativeWork'), test_should_be)
+        self.assertNotEqual(mapManifestOfItems(test_readfile, 'CreativeWorkSeries'), test_should_be)
 
 
 if __name__ == '__main__':
