59ab95d687d95bbfb8ab28b3c0b0d5d8feaf3f2e
Jon Hartzler <jon.hartzler@gmail.com>
2019-09-20 16:15:05 -0400

    add stop in test back in

--- local-deploy.sh
+++ local-deploy.sh
@@ -24,7 +24,7 @@ if [ -z ${S3_BUCKET+x} ]; then
 fi
 
 ./scripts/codebuild/install.sh  ||  { exit 1; }
-./scripts/codebuild/pre_build.sh # ||  { exit 1; }
+./scripts/codebuild/pre_build.sh ||  { exit 1; }
 ./scripts/codebuild/build.sh ||  { exit 1; }
 ./scripts/codebuild/post_build.sh ||  { exit 1; }
 
