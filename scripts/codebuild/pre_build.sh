b5dae2b504b6b7265d8f118914374828f404ce45
Steve Mattison <smattiso@nd.edu>
2019-09-27 14:17:31 -0400

    Added environment variables if missing for test

--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -4,5 +4,5 @@ echo "Copy the manifest-pipeline.yml to this project so it can run"
 echo "$BLUEPRINTS_DIR/deploy/cloudformation/manifest-pipeline.yml"
 cp $BLUEPRINTS_DIR/deploy/cloudformation/manifest-pipeline.yml ./
 
-
+pip install pyvips -t ./test
 python -m unittest discover  ||  { echo 'Auto Tests Failed' ; exit 1; }
