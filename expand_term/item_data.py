e5daa146725872086af70ffba3f30838105a1cb1
jmille79 <jmille79@nd.edu>
2019-10-14 14:38:23 -0400

    update

--- expand_term/item_data.py
+++ expand_term/item_data.py
@@ -1,11 +1,11 @@
-from conaiagen import get_conaia_list
-from locgen import get_loc_list
-from termgen import get_term_list
+from cona_ia_list import get_cona_ia_list
+from loc_list import get_loc_list
+from term_list import get_term_list
 
 
-def get_list(uri):
+def get_item_data(uri):
     if("http://www.getty.edu/cona/" in uri):
-        getresponse = get_conaia_list(uri)
+        getresponse = get_cona_ia_list(uri)
     elif("http://www.loc.gov/item/" in uri):
         getresponse = get_loc_list(uri)
     elif("vocab.getty.edu/" in uri):
