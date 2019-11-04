e6eb30b1d04e3d98fe9969ed8d29c03d1fc425fd
Jon Hartzler <jon.hartzler@gmail.com>
2019-11-04 14:40:58 -0500

    finalized the script changes.

--- scripts/restart_pipelines.py
+++ scripts/restart_pipelines.py
@@ -2,19 +2,27 @@
 # future could read this out of ssm
 import boto3
 import json
+import time
 
 # state machine to restart
-statemachine_arn = "arn:aws:states:us-east-1:333680067100:stateMachine:manifest-pipeline-mets-test-SchemaStateMachine"
+statemachine_arn = "arn:aws:states:us-east-1:230391840102:stateMachine:marble-manifest-prod-SchemaStateMachine"
 # bucket to find process items
-bucket = "manifest-pipeline-mets-test-processbucket-uryjoy854u02"
+bucket = "marble-manifest-prod-processbucket-kskqchthxshg"
 
 client = boto3.client('stepfunctions')
 s3 = boto3.resource('s3')
 result = s3.meta.client.list_objects(Bucket=bucket, Prefix="process/", Delimiter='/')
+count = 1
 
 for o in result.get('CommonPrefixes'):
     id = o.get('Prefix').replace("process/", "").replace("/", "")
     print("Starting:", id)
+    if count % 5 == 0:
+        print("sleeping")
+        time.sleep(150)
+
+    count += 1
+
     response = client.start_execution(
         stateMachineArn=statemachine_arn,
         input=json.dumps({"id": id})
