e3f50cf9de540437bdc3c8a3bb8818fa814845c0
Jon Hartzler <jon.hartzler@gmail.com>
2020-01-17 13:42:52 -0500

    try a new setup file

--- shared/setup.py
+++ shared/setup.py
@@ -1,1 +1,10 @@
+from distutils.core import setup
 
+setup(name='Pipeline utils',
+      version='1.0',
+      description='Manifest Pipeline Utilities',
+      author='Hesburgh Utilities',
+      author_email='jhartzle@nd.edu',
+      url='',
+      packages=['distutils', 'distutils.command'],
+     )
