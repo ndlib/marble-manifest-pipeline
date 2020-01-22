6b47aa02cdcbd89191d4c1d1c4c82a3ec02e287d
Jon Hartzler <jon.hartzler@gmail.com>
2020-01-22 15:04:33 -0500

    add another test and update some of the documentation.

--- init/helpers.py
+++ init/helpers.py
@@ -1,8 +1,11 @@
+# files to help the init action
 from datetime import datetime, timedelta, timezone
 import os
 
 
 def get_file_ids_to_be_processed(files, config):
+    """ looks at the file list from s3 and decides which file ids need to be processed """
+
     time_threshold_for_processing = determine_time_threshold_for_processing(config)
     for file in files:
         # if it is not the basedirectory which is returned in the list and
@@ -12,6 +15,8 @@ def get_file_ids_to_be_processed(files, config):
 
 
 def determine_time_threshold_for_processing(config):
+    """ Creates the datetime object that is used to test all the files against """
+
     time_threshold_for_processing = datetime.utcnow() - timedelta(hours=config['hours-threshold-for-incremental-harvest'])
     # since this is utc already but there is no timezone add it in so
     # the data can be compared to the timze zone aware date in file
@@ -19,6 +24,7 @@ def determine_time_threshold_for_processing(config):
 
 
 def get_if_from_file_key(key):
+    """ processes the file key to get the file id """
     # remove the extension
     id = os.path.splitext(key)
     # get the basename (filename)
