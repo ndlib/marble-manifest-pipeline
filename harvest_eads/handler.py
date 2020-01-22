c257cd2ac3d45bfcd838ba75d5cab485b39a0cb2
Steve Mattison <smattiso@nd.edu>
2020-01-22 11:27:04 -0500

    added harvest-eads

---
+++ harvest_eads/handler.py
@@ -0,0 +1,108 @@
+# handler.py
+""" Module to launch application """
+
+import json
+# import requests
+import io
+import os
+import sys
+from pathlib import Path
+where_i_am = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(where_i_am)
+sys.path.append(where_i_am + "/dependencies")
+from harvest_oai_eads import HarvestOaiEads  # noqa: #502
+from file_system_utilities import delete_file  # noqa: E402
+from dependencies.pipelineutilities.pipeline_config import get_pipeline_config  # noqa: E402
+import dependencies.sentry_sdk as sentry_sdk  # noqa: E402
+from dependencies.sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration  # noqa: E402
+
+
+def run(event, context):
+    """ Run the process to retrieve and process ArchivesSpace metadata.
+
+    Information on the API can be found here:
+        http://archivesspace.github.io/archivesspace/api/ """
+    _suplement_event(event)
+    _init_sentry()
+    config = get_pipeline_config(event)
+    if config != {}:
+        resumption_token = event['resumptionToken']
+        harvest_oai_eads_class = HarvestOaiEads(config, event)
+        mode = _check_environment_variable('MODE', 'full')
+        if not event['eadHarvestComplete']:
+            resumption_token = harvest_oai_eads_class.harvest_relevant_eads(mode, resumption_token)
+        event['resumptionToken'] = resumption_token
+        event['eadHarvestComplete'] = (resumption_token is None)
+    return event
+
+
+def _suplement_event(event):
+    if 'eadHarvestComplete' not in event:
+        event['eadHarvestComplete'] = False
+    if 'resumptionToken' not in event:
+        event['resumptionToken'] = ""
+    if 'ids' not in event:
+        event['ids'] = []
+    if 'local' not in event:
+        event['local'] = True
+    if 'ssm_key_base' not in event and 'SSM_KEY_BASE' in os.environ:
+        event['ssm_key_base'] = os.environ['SSM_KEY_BASE']
+    else:
+        event['ssm_key_base'] = "xxx"
+    if 'local-path' not in event:
+        event['local-path'] = str(Path(__file__).parent.absolute()) + "/../example/"
+    return
+
+
+def _init_sentry():
+    if 'SENTRY_DSN' in os.environ:
+        sentry_sdk.init(
+            dsn=os.environ['SENTRY_DSN'],
+            integrations=[AwsLambdaIntegration()]
+            )
+
+
+def _check_environment_variable(variable_name, default):
+    return_value = default
+    if variable_name in os.environ:
+        return_value = os.environ[variable_name]
+    return return_value
+
+
+# setup:
+# aws-vault exec testlibnd-superAdmin --session-ttl=1h --assume-role-ttl=1h --
+# aws-vault exec libnd-dlt-admin --session-ttl=1h --assume-role-ttl=1h --
+# export SSM_MARBLE_DATA_PROCESSING_KEY_BASE=/all/marble-data-processing/test
+# export SSM_KEY_BASE=/all/manifest-pipeline-v3
+
+# # export SSM_KEY_BASE=/all/marble-data-processing/test
+# export MODE=incremental
+# to run an individual ead, set the export to export MODE=individual and run the following:
+# python -c 'from handler import *; test()'
+# export HOURS_THRESHOLD=125
+# export SECONDS_TO_ALLOW_FOR_PROCESSING=1800
+# export REPOSITORIES_OF_INTEREST=["3"]
+# python -c 'from handler import *; test()'
+# python 'run_all_tests.py'
+def test(identifier=""):
+    """ test exection """
+    filename = 'event.json'
+    if os.path.exists(filename):
+        with io.open(filename, 'r', encoding='utf-8') as json_file:
+            event = json.load(json_file)
+    else:
+        event = {}
+        mode = _check_environment_variable('MODE', 'full')
+        if mode == 'identifiers':
+            event['ids'] = ['oai:und//repositories/3/resources/1644']
+        elif mode == 'ids':
+            event['ids'] = ['MSNEA8011_EAD']
+
+    event = run(event, {})
+
+    if not event['eadHarvestComplete']:
+        with open('event.json', 'w') as f:
+            json.dump(event, f, indent=2)
+    else:
+        delete_file('.', 'event.json')
+    print(event)
---
+++ harvest_eads/nodes_exist_in_json_tree.py
@@ -0,0 +1,52 @@
+
+
+def nodes_exist_in_json_tree(json_tree, required_descendants):
+    """ Returns boolean if all required nodes exists at any level in the passed json_tree """
+    nodes_exist = True
+    for required_descendant in required_descendants:
+        if not node_exists_in_json_tree(json_tree, required_descendant):
+            nodes_exist = False
+            break
+    return nodes_exist
+
+
+def node_exists_in_dict(dict_passed, node_name):
+    """ Test if one node exists anywhere in the dictionary passed """
+    if node_name in dict_passed:
+        return True
+    else:
+        for key, value in dict_passed.items():
+            result = node_exists_in_json_tree(value, node_name)
+            if result:
+                return result
+    return False
+
+
+def node_exists_in_json_tree(json_tree, node_name):
+    """ Test if one node exists anywhere in the json_tree """
+    if json_tree is None:
+        return False
+    if isinstance(json_tree, str):
+        return False
+    if isinstance(json_tree, dict):
+        result = node_exists_in_dict(json_tree, node_name)
+        if result:
+            return result
+    if isinstance(json_tree, list):
+        for item in json_tree:
+            result = node_exists_in_json_tree(item, node_name)
+            if result:
+                return result
+    return False
+
+
+# python -c 'from nodes_exist_in_json_tree import *; test()'
+def test():
+    """ test exection """
+    json_tree = {}
+    json_tree = {'abc': False, 'xyz': True}
+    json_tree = {'def': [{'abc': False, 'xyz': True}]}
+    json_tree = {'def': [{'xyz': True}, {'abc': False, 'xyz': False}, {'a': 'b'}]}
+    json_tree = {'level': ['file'], 'scopecontent': ['ALS. In this short note to the unidentified "Mr. Gardner," Thomas Hardy indicates he will not be able to join a meeting of the "friends of parliamentary reform."'], 'unittitle': ['Letter. Thomas Hardy, n.p., to "Mr. Gardner," n.p.'], 'unitid': ['MSE/MD 3811-9'], 'physdesc': ['1 page on 1 sheet.'], 'unitdate': ['1821 April 2'], 'ark': ['https://archivesspace.library.nd.edu/ark:/56090/429098'], 'container': [{'parent': ['1'], 'type': ['Box'], 'label': ['Mixed Materials'], 'id': ['aspace_83f1317a50e9c57d7a3f4b6bbecf3886'], 'text': ['1']}, {'parent': ['aspace_83f1317a50e9c57d7a3f4b6bbecf3886'], 'type': ['Folder'], 'label': ['9'], 'id': ['aspace_69f492cfa1c7a692c0366bdae8bdbf74'], 'text': ['9']}], 'digitalAsset': [{'desc': ['Letter. Thomas Hardy, n.p., to Mr. Gardner; n.p., Page 1'], 'href': ['https://rarebooks.library.nd.edu/collections/ead_xml/images/MSE-MD_3811/MSE-MD_3811-09.a.150.jpg']}, {'desc': ['Letter. Thomas Hardy, n.p., to Mr. Gardner; n.p., Page 2'], 'href': ['https://rarebooks.library.nd.edu/collections/ead_xml/images/MSE-MD_3811/MSE-MD_3811-09.b.150.jpg']}]}  # noqa: #501
+    required_descendants = ['digitalAsset']
+    print(nodes_exist_in_json_tree(json_tree, required_descendants))
