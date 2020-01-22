2442e3ca4c9db09b71be1d30fdf5812fc52687ae
Jon Hartzler <jon.hartzler@gmail.com>
2020-01-22 15:05:50 -0500

    hound

--- pipelineutilities/test/test_csv_collection.py
+++ pipelineutilities/test/test_csv_collection.py
@@ -6,11 +6,12 @@ sys.path.append(where_i_am + "/../")
 from pipelineutilities.csv_collection import Item
 
 objects = [
-    {"sourceSystem": "EmbARK", "repository": "repository", "myId": "collectionId", "collectionId": "collectionId", "parentId": "root", "level": "collection" },
-    {"sourceSystem": "", "repository": "", "myId": "itemId", "collectionId": "collectionId", "parentId": "collectionId", "level": "manifest" },
-    {"sourceSystem": "", "repository": "", "myId": "fileId1", "collectionId": "collectionId", "parentId": "itemId", "level": "manifest" },
-    {"sourceSystem": "", "repository": "", "myId": "fileId2", "collectionId": "collectionId", "parentId": "itemId", "level": "manifest" }
+    {"sourceSystem": "EmbARK", "repository": "repository", "myId": "collectionId", "collectionId": "collectionId", "parentId": "root", "level": "collection"},
+    {"sourceSystem": "", "repository": "", "myId": "itemId", "collectionId": "collectionId", "parentId": "collectionId", "level": "manifest"},
+    {"sourceSystem": "", "repository": "", "myId": "fileId1", "collectionId": "collectionId", "parentId": "itemId", "level": "manifest"},
+    {"sourceSystem": "", "repository": "", "myId": "fileId2", "collectionId": "collectionId", "parentId": "itemId", "level": "manifest"}
 ]
+
 
 class TestCsvCollection(unittest.TestCase):
 
