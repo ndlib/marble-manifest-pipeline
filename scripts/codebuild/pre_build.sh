111d9412a15a5808b64dd73b4ec2d76c6f623a56
Ryan Doughty <rdought1@nd.edu>
2020-01-31 14:11:21 -0500

    enable entrypoint and test

--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -4,5 +4,5 @@ echo "Copy the manifest-pipeline.yml to this project so it can run"
 echo "$BLUEPRINTS_DIR/deploy/cloudformation/manifest-pipeline.yml"
 cp $BLUEPRINTS_DIR/deploy/cloudformation/manifest-pipeline.yml ./
 
-#python -m unittest discover ./test  ||  { echo 'Auto Tests Failed' ; exit 1; }
-#python -m unittest discover ./pipelineutilities/test  ||  { echo 'Auto Tests Failed' ; exit 1; }
+python -m unittest discover ./test  ||  { echo 'Auto Tests Failed' ; exit 1; }
+python -m unittest discover ./pipelineutilities/test  ||  { echo 'Auto Tests Failed' ; exit 1; }
