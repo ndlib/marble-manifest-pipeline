dc1b88cd8a794db9045667cc738f7500142b0e8f
Ryan Doughty <rdought1@nd.edu>
2020-02-21 10:29:19 -0500

    forgotten wheels

--- pipelineutilities/build/lib/pipelineutilities/s3_helpers.py
+++ pipelineutilities/build/lib/pipelineutilities/s3_helpers.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+from os.path import basename
+from os import remove
 
 
 def get_matching_s3_objects(bucket, prefix="", suffix=""):
@@ -120,7 +122,19 @@ def s3_copy_data(dest_bucket, dest_key, src_bucket, src_key, **kwargs):
 
 
 def delete_file(s3Bucket, s3Path):
-    boto3.client('s3').delete_object(
+    return boto3.client('s3').delete_object(
         Bucket=s3Bucket,
         Key=s3Path,
     )
+
+
+def upload_json(s3Bucket, s3Path, json_data) -> None:
+    local_file = f"/tmp/{basename(s3Path)}"
+    with open(local_file, 'w') as outfile:
+        json.dump(json_data, outfile)
+    upload_file(s3Bucket, s3Path, local_file)
+    remove(local_file)
+
+
+def upload_file(s3Bucket, s3Path, local_file):
+    boto3.resource('s3').Bucket(s3Bucket).upload_file(local_file, s3Path)
