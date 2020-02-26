35f08b130d57b1ca4081bb6bf3becd2487a5893a
Steve Mattison <smattiso@nd.edu>
2020-02-26 13:26:55 -0500

    added to aleph handler too

--- aleph_export/handler.py
+++ aleph_export/handler.py
@@ -3,6 +3,10 @@
 
 import os
 from pathlib import Path
+import sys
+where_i_am = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(where_i_am)
+sys.path.append(where_i_am + "/dependencies/")
 from harvest_aleph_marc import HarvestAlephMarc  # noqa: #502
 from dependencies.pipelineutilities.pipeline_config import get_pipeline_config  # noqa: E402
 import dependencies.sentry_sdk as sentry_sdk  # noqa: E402
