ae793d4131c681ca7e0ff9202c5fc533748f2d77
Jon Hartzler <jon.hartzler@gmail.com>
2020-05-05 15:46:59 -0400

    wheel

--- pipelineutilities/build/lib/pipelineutilities/load_nd_json.py
+++ pipelineutilities/build/lib/pipelineutilities/load_nd_json.py
@@ -4,16 +4,80 @@ import boto3
 import json
 
 
+def load_image_data(id, config):
+    if config.get('local', False):
+        return load_image_from_file(id, config)
+    else:
+        return load_image_from_s3(config['process-bucket'], config['process-bucket-read-basepath'], id, config)
+
+
+def load_image_from_file(id, config):
+    filepath = config['local-path'] + "/" + id + "/" + config['image-data-file']
+    try:
+        with open(filepath, 'r') as input_source:
+            source = input_source.read()
+        input_source.close()
+    except FileNotFoundError:
+        return {}
+
+    return json.loads(source)
+
+
+def load_image_from_s3(s3Bucket, s3Path, id, config):
+    s3Path = s3Path + "/" + id + "/" + config['image-data-file']
+
+    try:
+        content_object = boto3.resource('s3').Object(s3Bucket, s3Path)
+        source = content_object.get()['Body'].read().decode('utf-8')
+        return json.loads(source)
+    except boto3.resource('s3').meta.client.exceptions.NoSuchKey:
+        return {}
+
+
 def load_nd_json(id, config):
     if config.get('local', False):
         tree = _load_json_from_file(id, config['local-path'])
     else:
         tree = _load_json_from_s3(config['process-bucket'], 'json', id)
+
+    images = load_image_data(id, config)
+    _augment_row_data(tree, images, config)
+
     return Item(tree, tree)
 
 
+def _augment_row_data(tree, all_image_data, config):
+    _add_image_dimensions(tree, all_image_data, config)
+
+    for item in tree.get('items', []):
+        _augment_row_data(item, all_image_data, config)
+
+
+def _add_image_dimensions(row, all_image_data, config):
+    level = row.get('level')
+    if level != "file":
+        row.update({"width": None, "height": None})
+        return
+
+    image_key = os.path.splitext(row.get('id'))[0]
+    image_data = all_image_data.get(image_key, {})
+
+    ret = {
+        "height": image_data.get('height', False),
+        "width": image_data.get('width', False)
+    }
+
+    if not ret["height"]:
+        ret["height"] = config['canvas-default-height']
+
+    if not ret["width"]:
+        ret["width"] = config['canvas-default-width']
+
+    row.update(ret)
+
+
 def _load_json_from_file(id, local_path):
-    filepath = os.path.join(local_path, 'json', id + ".json")
+    filepath = os.path.join(local_path, id, id + ".json")
     try:
         with open(filepath, 'r') as input_source:
             source = input_source.read()
@@ -27,7 +91,6 @@ def _load_json_from_file(id, local_path):
 def _load_json_from_s3(s3_bucket, s3_path, id):
     s3_path = os.path.join(s3_path, id + ".json")
 
-    print(s3_bucket, s3_path)
     try:
         return read_s3_json(s3_bucket, s3_path)
     except boto3.resource('s3').meta.client.exceptions.NoSuchKey:
