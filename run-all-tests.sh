b533a8b95081e6e2c49c48f96230b050688e7ae9
Jon Hartzler <jon.hartzler@gmail.com>
2020-05-05 15:58:56 -0400

    reenable build code for travis

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,4 +1,4 @@
-#pip uninstall pipelineutilities
+pip uninstall pipelineutilities
 
-#./scripts/codebuild/install.sh  ||  { exit 1; }
+./scripts/codebuild/install.sh  ||  { exit 1; }
 ./scripts/codebuild/pre_build.sh ||  { exit 1; }
