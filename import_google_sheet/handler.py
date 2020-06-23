105e058c35b21c2935014845629f3e2f45e5a8d3
Steve Mattison <smattiso@nd.edu>
2020-06-23 16:08:04 -0400

    copy metadata information from google sheet
    * Created new import_google_sheet module.
    * Created marble.json to define spreadsheet id to harvest, as well as column names to harvest, and the column to use as the key
    * This creates a folder in the local ./sites folder for each event["sites"] json file listed (currently only ["marble"].
    * Each tab in the spreadsheet is harvested, with resulting json file saved in the appropriate "site" folder (e.g. "marble"), with json for each tab named after the tab (e.g. "embark.json").
    * A copy of these json files are copied both to the process bucket and to the process_manifest folder for future use there.

--- import_google_sheet/handler.py
+++ import_google_sheet/handler.py
@@ -3,12 +3,15 @@
 
 import _set_path  # noqa
 import os
-# import json
+import json
+import io
 from pathlib import Path
 from pipelineutilities.pipeline_config import setup_pipeline_config, load_config_ssm  # noqa: E402
 import sentry_sdk  # noqa: E402
 from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration  # noqa: E402
 from harvest_google_sheet_info import HarvestGoogleSheetInfo
+from dependencies.sentry_sdk import capture_exception
+from distutils.dir_util import copy_tree
 
 
 if 'SENTRY_DSN' in os.environ:
@@ -21,10 +24,26 @@ def run(event, context):
     config = setup_pipeline_config(event)
     google_config = load_config_ssm(config['google_keys_ssm_base'])
     config.update(google_config)
-    harvest_google_sheet_info_class = HarvestGoogleSheetInfo(config)
-    google_sheet_id = "1gKUkoG921EW0AAa-9c58Yn3wGyx8UXLnlFCWHs3G7E4"
-    sheet_json = harvest_google_sheet_info_class.harvest_google_sheet_info(google_sheet_id)
-    print("sheet_json = ", sheet_json)
+    google_credentials = json.loads(config["museum-google-credentials"])
+    harvest_google_sheet_info_class = HarvestGoogleSheetInfo(google_credentials)
+    local_folder = os.path.dirname(os.path.realpath(__file__)) + '/'
+    for site_name in event['sites']:
+        filename = local_folder + site_name + '.json'
+        try:
+            with io.open(filename, 'r', encoding='utf-8') as json_file:
+                site_control_json = json.load(json_file)
+        except Exception as e:
+            print('Unable to open file ' + filename)
+            capture_exception(e)
+            break
+        harvest_google_sheet_info_class.harvest_google_spreadsheet_info(site_name, site_control_json['googleSpreadsheetId'], site_control_json['columnsToExport'], site_control_json['fieldForKey'])
+    _save_sites_info_to_s3(local_folder + "sites/", "s3://" + config["process-bucket"] + "/sites/")
+    copy_tree(local_folder + "sites/", local_folder + "../process_manifest/sites/")
+
+
+def _save_sites_info_to_s3(sites_folder, s3_key):
+    sync_command = f"aws s3 sync " + sites_folder + " " + s3_key
+    os.system(sync_command)
 
 
 def _suplement_event(event):
@@ -32,16 +51,19 @@ def _suplement_event(event):
         event['ssm_key_base'] = os.environ['SSM_KEY_BASE']
     if 'local-path' not in event:
         event['local-path'] = str(Path(__file__).parent.absolute()) + "/../example/"
+    if 'sites' not in event:
+        event['sites'] = ['marble']  # assume we always want to automatically capture at least marble site info
     return
 
 
 # setup:
-# cd museum_export
-# aws-vault exec testlibnd-superAdmin --session-ttl=1h --assume-role-ttl=1h --
+# cd import_google_sheet
 # export SSM_KEY_BASE=/all/new-csv
 # python -c 'from handler import *; test()'
+# aws-vault exec testlibnd-superAdmin --session-ttl=1h --assume-role-ttl=1h --
 # python 'run_all_tests.py'
 def test():
     """ test exection """
     event = {}
+    event['sites'] = ['marble']
     run(event, {})
