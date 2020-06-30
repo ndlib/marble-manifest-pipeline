880944109e01631224261dee19f749c39798045d
Steve Mattison <smattiso@nd.edu>
2020-06-30 09:01:58 -0400

    Modified museum export to be able to resume processing
    * Added copyright info to museum files metadata
    * Modified museum export to stash inermediate results and resume processing on subsequent lambda calls.
    * Added tests to museum export.
    * Moified pipelineutilities/record_files_needing_processed to include try..except.
    * Modified pipelineutilities/save_standard_json to return success_flag indicating if the standard_json was actually saved or not.

--- museum_export/process_one_museum_object.py
+++ museum_export/process_one_museum_object.py
@@ -1,35 +1,32 @@
 # process_one_museum_object.py
 """ Module to do processing for one museum object """
-import os
 import time
 import json
 import copy
+import os
 from clean_up_content import CleanUpContent
-from convert_json_to_csv import ConvertJsonToCsv
 from dependencies.pipelineutilities.validate_json import validate_json, get_standard_json_schema, schema_api_version
-from pipelineutilities.s3_helpers import write_s3_file
 from sentry_sdk import capture_message, push_scope
-# from pipelineutilities.add_files_to_json_object import AddFilesToJsonObject
 from pipelineutilities.add_paths_to_json_object import AddPathsToJsonObject
 from pipelineutilities.fix_creators_in_json_object import FixCreatorsInJsonObject
-from pipelineutilities.save_standard_json import save_standard_json
+
+local_folder = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 
 class ProcessOneMuseumObject():
-    def __init__(self, config: dict, image_files: dict, start_time: str, event: dict):
+    def __init__(self, config: dict, image_files: dict, start_time: str):
         self.config = config
         self.image_files = image_files
         self.start_time = start_time
-        self.event = event
         self.save_despite_missing_fields = True
         self.api_version = schema_api_version()
-        # self.add_files_to_json_object_class = AddFilesToJsonObject(config)
         self.add_paths_to_json_object_class = AddPathsToJsonObject(config)
         self.fix_creators_in_json_object_class = FixCreatorsInJsonObject(config)
 
-    def process_object(self, museum_object: dict):
+    def process_object(self, museum_object: dict) -> dict:
         """ For each object, check for missing fields.  If there are none,
-            save information as CSV to S3, and delete the local copy. """
+            generate standard json. """
+        standard_json = {}
         object_id = museum_object['uniqueIdentifier']
         missing_fields = self._test_for_missing_fields(object_id, museum_object, self.config['museum-required-fields'])
         if missing_fields == "" or self.save_despite_missing_fields:
@@ -39,17 +36,12 @@ class ProcessOneMuseumObject():
             clean_up_content_class = CleanUpContent(self.config, self.image_files, self.api_version)
             standard_json = clean_up_content_class.clean_up_content(museum_object)
             if not validate_standard_json(standard_json):
-                print("Validation Error validating modified object", object_id)
+                print("Validation Error validating cleaned up content standard_json", object_id)
+                standard_json = {}
             else:
-                # standard_json = self.add_files_to_json_object_class.add_files(standard_json)
                 standard_json = self.add_paths_to_json_object_class.add_paths(standard_json)
                 standard_json = self.fix_creators_in_json_object_class.fix_creators(standard_json)
-            export_all_files_flag = self.event.get('export_all_files_flag', False)
-            save_standard_json(self.config, standard_json, export_all_files_flag)
-            # with open("standard_json.json", 'w') as f:
-            #     json.dump(standard_json, f, indent=4)
-            self._save_csv(object_id, standard_json)
-        return missing_fields
+        return standard_json
 
     def _test_for_missing_fields(self, object_id: dict, json_object: dict, required_fields: dict) -> str:
         """ Test for missing required fields """
@@ -76,25 +68,6 @@ class ProcessOneMuseumObject():
             scope.level = 'warning'
             capture_message(object_id + ' is missing the following required field(s): \n' + missing_fields)
 
-    def _save_csv(self, object_id: str, object: dict):
-        """ save csv string as a csv file """
-        csv_file_name = object_id + '.csv'
-        convert_json_to_csv_class = ConvertJsonToCsv(self.config["csv-field-names"])
-        csv_string = convert_json_to_csv_class.convert_json_to_csv(object)
-        if self.config["local"]:
-            write_csv_locally(csv_file_name, csv_string)
-        else:
-            s3_csv_file_name = os.path.join(self.config['process-bucket-csv-basepath'], csv_file_name)
-            write_s3_file(self.config['process-bucket'], s3_csv_file_name, csv_string)
-
-
-def write_csv_locally(csv_file_name: str, csv_string: str):
-    """ save csv locally """
-    fully_qualified_file_name = os.path.join("./after_changes", csv_file_name)
-    with open(fully_qualified_file_name, 'w') as f:
-        f.write(csv_string)
-    return
-
 
 def validate_museum_json(json_to_test: dict) -> bool:
     """ validate json coming from EmbARK before processing it """
@@ -106,7 +79,7 @@ def validate_museum_json(json_to_test: dict) -> bool:
 def get_museum_json_schema() -> dict:
     """ get schema appropriate for checking EmbARK json """
     schema_to_use = copy.deepcopy(get_standard_json_schema())
-    with open('./museum_specific_schema_fields.json') as f:
+    with open(local_folder + 'museum_specific_schema_fields.json') as f:
         museum_specific_schema_fields = json.load(f)
     schema_to_use["required"] = ["id"]  # explicitly remove parentId and collectionId, since objects with hidden parents don't have these
     schema_to_use["properties"].update(museum_specific_schema_fields["properties"])
--- museum_export/process_web_kiosk_json_metadata.py
+++ museum_export/process_web_kiosk_json_metadata.py
@@ -8,11 +8,13 @@ import json
 from datetime import datetime, timedelta
 import time
 import os
+from convert_json_to_csv import ConvertJsonToCsv
 from dependencies.sentry_sdk import capture_exception
 import dependencies.requests
 from process_one_museum_object import ProcessOneMuseumObject
-from clean_up_composite_json import CleanUpCompositeJson
 from get_image_info_for_all_objects import GetImageInfoForAllObjects
+from pipelineutilities.save_standard_json import save_standard_json
+from pipelineutilities.s3_helpers import write_s3_file
 
 
 class processWebKioskJsonMetadata():
@@ -20,13 +22,14 @@ class processWebKioskJsonMetadata():
         Individual json files are created, one per object.
         These individual json files are saved locally by object name.
         They are then uploaded to a Google Team Drive, and deleted locally. """
-    def __init__(self, config: dict, event: dict):
+    def __init__(self, config: dict, event: dict, time_to_break: datetime):
         self.config = config
         self.event = event
+        self.time_to_break = time_to_break
         self.folder_name = "/tmp"
         self.file_name = 'web_kiosk_composite_metadata.json'
-        self.save_local_copy = True
-        self.delete_local_copy = True
+        self.save_local_copy = False
+        self.delete_local_copy = False
         self.start_time = time.time()
 
     def get_composite_json_metadata(self, mode: str) -> dict:
@@ -59,30 +62,39 @@ class processWebKioskJsonMetadata():
                         composite_json["objects"].update(this_composite_json["objects"])
         return composite_json
 
-    def process_composite_json_metadata(self, composite_json: dict, running_unit_tests: bool = False) -> int:
-        """ Split big composite metadata file into individual small metadata files """
-        objects_processed = 0
-        accumulated_missing_fields = ''
+    def find_images_for_composite_json_metadata(self, composite_json: dict) -> dict:
+        image_file_info = {}
         if 'objects' in composite_json:
             objects = composite_json["objects"]
-            google_credentials = json.loads(self.config["museum-google-credentials"])
-            drive_id = self.config['museum-google-drive-id']
+            google_credentials = {}
+            if self.config.get("museum-google-credentials", ""):
+                json.loads(self.config["museum-google-credentials"])
+            drive_id = self.config.get("museum-google-drive-id", "")
+            # image_file_info = GetImageInfoForAllObjects(objects, google_credentials, drive_id).image_file_info
+            # print("image_file_info = ", image_file_info)
+            # if google_credentials and drive_id:
+            print("before")
             image_file_info = GetImageInfoForAllObjects(objects, google_credentials, drive_id).image_file_info
             print("Completed retrieving Google image file info after", int(time.time() - self.start_time), 'seconds.')
-            composite_json = CleanUpCompositeJson(composite_json).cleaned_up_content
-            process_one_museum_object_class = ProcessOneMuseumObject(self.config, image_file_info, self.start_time, self.event)
+        return image_file_info
+
+    def process_composite_json_metadata(self, composite_json: dict, image_file_info: dict, running_unit_tests: bool = False) -> int:
+        """ Split big composite metadata file into individual small metadata files """
+        objects_processed = 0
+        if 'objects' in composite_json:
+            objects = composite_json["objects"]
+            export_all_files_flag = self.event.get('export_all_files_flag', False)
+            process_one_museum_object_class = ProcessOneMuseumObject(self.config, image_file_info, self.start_time)
             for _object_key, object_value in objects.items():
-                if 'uniqueIdentifier' in object_value:
-                    missing_fields = process_one_museum_object_class.process_object(object_value)
+                if 'uniqueIdentifier' in object_value and not object_value.get("recordProcessedFlag", False):
+                    standard_json = process_one_museum_object_class.process_object(object_value)
+                    save_standard_json(self.config, standard_json, export_all_files_flag)
+                    object_value["recordProcessedFlag"] = True
                     objects_processed += 1
-                    if missing_fields > '':
-                        accumulated_missing_fields += missing_fields
+                    if datetime.now() >= self.time_to_break:
+                        break
                     if running_unit_tests:
                         break
-            if accumulated_missing_fields > '':
-                # May consider here _log_missing_field oncd per run instead of once per object.
-                # self._log_missing_field("Call to Museum JSON harvest ", accumulated_missing_fields)
-                pass
         if self.delete_local_copy:
             delete_file(self.folder_name, self.file_name)
         if not self.event.get("local", False):
@@ -115,6 +127,25 @@ class processWebKioskJsonMetadata():
             recent_past_string = recent_past.strftime('%m/%d/%Y')
             url = base_url + "&query=mod_date%3E%22" + recent_past_string + "%22"
         return(url)
+
+    def _save_csv(self, object_id: str, object: dict):
+        """ save csv string as a csv file """
+        csv_file_name = object_id + '.csv'
+        convert_json_to_csv_class = ConvertJsonToCsv(self.config["csv-field-names"])
+        csv_string = convert_json_to_csv_class.convert_json_to_csv(object)
+        if self.config["local"]:
+            write_csv_locally(csv_file_name, csv_string)
+        else:
+            s3_csv_file_name = os.path.join(self.config['process-bucket-csv-basepath'], csv_file_name)
+            write_s3_file(self.config['process-bucket'], s3_csv_file_name, csv_string)
+
+
+def write_csv_locally(csv_file_name: str, csv_string: str):
+    """ save csv locally """
+    fully_qualified_file_name = os.path.join("./test", csv_file_name)
+    with open(fully_qualified_file_name, 'w') as f:
+        f.write(csv_string)
+    return
 
 
 def delete_file(folder_name: str, file_name: str):
