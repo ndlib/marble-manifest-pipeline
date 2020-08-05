8e6b0c06ba3820744f8177f2eb97ecee0e669ad5
Ryan Doughty <rdought1@nd.edu>
2020-08-05 11:34:23 -0400

    remove containerurl

--- local-deploy.sh
+++ local-deploy.sh
@@ -31,7 +31,6 @@ fi
 aws cloudformation deploy --template-file output.yml --stack-name $stackname \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides AppConfigPath="/all/$stackname" \
-    ContainerImageUrl="333680067100.dkr.ecr.us-east-1.amazonaws.com/marbl-image-1cs1q74l7njir:latest" \
     HostnamePrefix=$stackname \
     ImageServerHostname="/all/stacks/marble-image-service-prod/hostname" \
     MarbleProcessingKeyPath="/all/marble-data-processing/test" \
